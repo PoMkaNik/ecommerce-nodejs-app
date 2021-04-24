36b0d4c21fa0d7d5c400b02d25f76b3ab31aad68
Roman Dubovik <ip.romandubovik@gmail.com>
2021-04-24 14:32:29 +0300

    validation errors handling and check if user is authorized were moved to custom middlewares

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 // validators
 const {
   requireEmail,
@@ -8,6 +7,9 @@ const {
   requireEmailExists,
   requireValidPasswordForUser,
 } = require('./validators');
+// middlewares
+const { handleErrors } = require('./middlewares');
+
 // repositories
 const usersRepo = require('../../repositories/users');
 // views
@@ -24,17 +26,15 @@ router.post(
   '/signup',
   // sanitization and validation
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.send(signupTemplate({ req, errors }));
-
     const { email, password } = req.body;
 
     const user = await usersRepo.create({ email, password });
     // .session added by cookie-session
     req.session.userId = user.id;
 
-    res.send('Account created');
+    res.redirect('/admin/products');
   },
 );
 
@@ -51,15 +51,13 @@ router.post(
   '/signin',
   // sanitization and validation
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.send(signinTemplate({ errors }));
-
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
     req.session.userId = user.id;
 
-    res.send('You are signed in');
+    res.redirect('/admin/products');
   },
 );
 
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,37 +1,41 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 // middleware for file uploads
 const multer = require('multer');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 // settings for middleware
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', requireAuth, async (req, res) => {
+  const products = await productsRepo.getAll();
 
-router.get('/admin/products/new', (req, res) => {
+  res.send(productsIndexTemplate({ products }));
+});
+
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   '/admin/products/new',
   // the order is matter!
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
+  // handle validation errors middleware
+  handleErrors(productsNewTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) return res.send(productsNewTemplate({ errors }));
-
-    const image = req.file.buffer.toString('base64');
+    const image = req.file ? req.file.buffer.toString('base64') : '';
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
 
-    res.send('Submitted');
+    res.redirect('/admin/products');
   },
 );
 
